d2ab9145d7830fe0189fb55de9678e2bc14575ed
Afolabi Isaac O <afolabi.gbenga.231@gmail.com>
2022-10-14 21:03:58 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints char below ten
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int b;
+for (b = '0'; b < 10; b++)
+{
+putchar(b + '0');
+}
+putchar('\n');
+return (0);
+}
