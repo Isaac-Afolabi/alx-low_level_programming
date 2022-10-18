0f49493c866af20c2c31ce1defb12c49e2e84b54
Afolabi Isaac O <afolabi.gbenga.231@gmail.com>
2022-10-18 04:53:08 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - the entry point
+ * Prints _putchar, followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+This is a README file for 0x02-functions_nested_loops
+It contains C files: programs and functions pertaining to loops.
