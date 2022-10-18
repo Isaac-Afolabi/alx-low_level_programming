459cd7f38e0718907c028125e9cb3e615b3e7a1d
Afolabi Isaac O <afolabi.gbenga.231@gmail.com>
2022-10-18 07:31:43 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - the function to print abc in lower case
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+char p;
+for (p = 'a'; p <= 'z'; p++)
+{
+_putchar(p);
+}
+_putchar('\n');
+}
