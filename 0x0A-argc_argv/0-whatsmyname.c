2bed88fe4cdce4165aaed2a3c05c6e4ca74ec3c3
Afolabi Isaac O <afolabi.gbenga.231@gmail.com>
2022-11-04 22:20:21 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Prints its name then /n
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]);
+return (0);
+}
